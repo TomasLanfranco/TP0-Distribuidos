8ef64951638b95a16ac10b85d90b3466615dbefa
TomasLanfranco <tomilanfranco04@gmail.com>
2025-08-23 23:47:07 +0000

    feat: creo script para generar docker compose

---
+++ generar-compose.py
@@ -0,0 +1,98 @@
+import sys
+
+COMPOSE_NAME = 'tp0'
+NETWORK_NAME = 'testing_net'
+SERVER_LOG_LEVEL = 'DEBUG'
+CLIENT_LOG_LEVEL = 'DEBUG'
+NARGS = 3
+
+
+def format_dict_to_yaml(d, indentation=0):
+    compose = ''
+    for key, value in d.items():
+        compose += f"{'  ' * indentation}{key}:"
+        if isinstance(value, dict):
+            compose += "\n"
+            compose += format_dict_to_yaml(value, indentation + 1)
+        elif isinstance(value, list):
+            compose += "\n"
+            for item in value:
+                compose += f"{'  ' * (indentation + 1)}- {item}\n"
+        else:
+            compose += f" {value}\n"
+    return compose
+
+
+def build_server_definition():
+    server = {
+        'container_name': 'server',
+        'image': 'server:latest',
+        'entrypoint': 'python3 /main.py',
+        'environment': [
+            'PYTHONUNBUFFERED=1',
+            f'LOGGING_LEVEL={SERVER_LOG_LEVEL}'
+        ],
+        'networks': [NETWORK_NAME]
+    }
+    return format_dict_to_yaml({'server': server}, 1)
+
+
+def build_client_definition(n):
+    client = {
+        'container_name': f'client{n}',
+        'image': 'client:latest',
+        'entrypoint': '/client',
+        'environment': [
+            f'CLI_ID={n}',
+            f'CLI_LOG_LEVEL={CLIENT_LOG_LEVEL}'
+        ],
+        'networks': [NETWORK_NAME],
+        'depends_on': ['server']
+    }
+    return format_dict_to_yaml({f'client{n}': client}, 1)
+
+
+def build_networks_definition():
+    networks = {
+        NETWORK_NAME: {
+            'ipam': {
+                'driver': 'default',
+                'config': [
+                    'subnet: 172.25.125.0/24'
+                ]
+            }
+        }
+    }
+    return format_dict_to_yaml({'networks': networks})
+
+
+def build_compose(client_n):
+    compose = ''
+    compose += f'name: {COMPOSE_NAME}\n'
+    compose += f'services:\n'
+
+    compose += build_server_definition()
+
+    for i in range(1, int(client_n) + 1):
+        compose += "\n"
+        compose += build_client_definition(i)
+
+    compose += "\n"
+    compose += build_networks_definition()
+
+    return compose
+
+
+def main():
+    if len(sys.argv) != NARGS:
+        print("Usage: python3 generar-compose.py <num_clients> <output_file>")
+        sys.exit(1)
+
+    _, output_file, client_n = sys.argv
+
+    with open(output_file, 'w') as docker_compose_yaml:
+        docker_compose_yaml.write(build_compose(client_n))
+
+
+if __name__ == "__main__":
+    main()
