98e8304798822ccf78ed40240de3472527dda0ec
TomasLanfranco <tomilanfranco04@gmail.com>
2025-08-24 03:29:01 +0000

    fix: saco argumentos del script de validacion

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,12 +1,6 @@
 #! /usr/bin/bash
 
-NARGS=1
-if [ $# -ne $NARGS ]; then
-    echo "Usage: ./validar-echo-server.sh NETWORK_NAME"
-    exit 1
-fi
-
-NETWORK_NAME=$1
+NETWORK_NAME="tp0_testing_net"
 EXPECTED='Hola mundo'
 RESPONSE=$(docker run --rm --network $NETWORK_NAME busybox sh -c "echo '$EXPECTED'| nc server 12345")
 if [ "$RESPONSE" = "$EXPECTED" ]; then
