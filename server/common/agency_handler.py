3ed3fbd5829a45c9bc8250d4396bcfa3229e0ce4
TomasLanfranco <tomilanfranco04@gmail.com>
2025-09-04 13:26:12 +0000

    fix

--- server/common/agency_handler.py
+++ server/common/agency_handler.py
@@ -17,22 +17,21 @@ class AgencyHandler(threading.Thread):
 
 
     def run(self):
-        try:
-            agency = self.__process_batches()
-            if agency == -2:
-                self.__send_ack(None)  # send ack with number 0 to indicate stop to client
-                self.__notify_ready()
-            if agency > -2:
-                logging.info(f"action: receive_message | result: success | agency: {agency}")
-                self.__notify_server(agency)
-            if agency > -1:
-                logging.info("action: wait_winners | result: in_progress")
-                winners = self.q.get()
-                self.__send_winners(winners)
-        except Exception as e:
-            logging.error(f"action: receive_message | result: fail | error: {e}")
-        finally:
-            self.client_socket.close()
+        agency = self.__process_batches()
+        if agency == -2:
+            self.__send_ack(None)  # send ack with number 0 to indicate stop to client
+            self.__notify_ready()
+        if agency > -2:
+            logging.info(f"action: receive_message | result: success | agency: {agency}")
+            self.__notify_server(agency)
+        if agency > -1:
+            logging.info("action: wait_winners | result: in_progress")
+            winners = self.q.get()
+            self.__send_winners(winners)
+        self.client_socket.close()
+        # try:
+        # except Exception as e:
+        # finally:
 
 
     def __notify_server(self, agency):
